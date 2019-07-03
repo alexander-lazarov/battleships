51a8f528f9baea1176b284f5a578d3db914fe10a
Alexander Lazarov <alexander.lazaroff@gmail.com>
2019-07-04 00:15:14 +0300

    Fix failing test

--- test/battleships_web/controllers/page_controller_test.exs
+++ test/battleships_web/controllers/page_controller_test.exs
@@ -1,6 +1,16 @@
 defmodule BattleshipsWeb.PageControllerTest do
   use BattleshipsWeb.ConnCase
 
+  setup do
+    {:ok, _} = Application.ensure_all_started(:battleships)
+
+    on_exit(fn ->
+      Application.stop(:battleships)
+    end)
+
+    :ok
+  end
+
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
