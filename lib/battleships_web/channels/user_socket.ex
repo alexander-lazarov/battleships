5a21187293bdaf4f52fba7b07e067391e5aaefb5
Alexander Lazarov <alexander.lazaroff@gmail.com>
2019-07-04 22:07:42 +0300

    Set up Vue.js and channels

--- lib/battleships_web/channels/user_socket.ex
+++ lib/battleships_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule BattleshipsWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", BattleshipsWeb.RoomChannel
+  channel "battleships:join", BattleshipsWeb.RoomChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
