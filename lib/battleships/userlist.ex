b68c704b1c05354de843bede28029367a378a48f
Alexander Lazarov <alexander.lazaroff@gmail.com>
2019-07-05 01:07:05 +0300

    Implement super basic userlist server

--- lib/battleships/userlist.ex
+++ lib/battleships/userlist.ex
@@ -1,19 +1,34 @@
 defmodule Battleships.Userlist do
   use GenServer
 
+  @type state :: MapSet.t()
+  @type userlist :: list(String.t())
+
   def init(_) do
-    {:ok, []}
+    {:ok, MapSet.new()}
   end
 
+  @spec start_link(any()) :: {:ok, pid()}
   def start_link(_) do
     GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
+  @spec get() :: userlist()
   def get() do
     GenServer.call(__MODULE__, :get)
   end
 
-  def handle_call(:get, _from, state) do
-    {:reply, state, state}
+  @spec add(String.t()) :: :ok | :in_use
+  def add(new_user), do: GenServer.call(__MODULE__, {:add, new_user})
+
+  @spec handle_call(:get, any(), state()) :: {:reply, userlist(), state()}
+  def handle_call(:get, _from, state), do: {:reply, state |> MapSet.to_list(), state}
+
+  @spec handle_call({:add, String.t()}, any(), state()) :: {:reply, :ok | :in_use, state()}
+  def handle_call({:add, new_user}, _from, state) do
+    case state |> MapSet.member?(new_user) do
+      true -> {:reply, :in_use, state}
+      false -> {:reply, :ok, state |> MapSet.put(new_user)}
+    end
   end
 end
--- test/battleship/userlist_test.exs
+++ test/battleship/userlist_test.exs
@@ -14,4 +14,15 @@ defmodule Battleship.UserlistTest do
   test "the list is empty initially" do
     assert Battleships.Userlist.get() == []
   end
+
+  test "that you can insert users" do
+    assert :ok == Battleships.Userlist.add("user1")
+    assert Battleships.Userlist.get() == ["user1"]
+  end
+
+  test "that you cannot insert the same user twice" do
+    assert :ok == Battleships.Userlist.add("user1")
+    assert :in_use == Battleships.Userlist.add("user1")
+    assert Battleships.Userlist.get() == ["user1"]
+  end
 end
