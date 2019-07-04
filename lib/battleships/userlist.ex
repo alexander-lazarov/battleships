2eb33ab498f0278825f6459dfa2fb9e967cc63f0
Alexander Lazarov <alexander.lazaroff@gmail.com>
2019-07-05 01:16:20 +0300

    Cleanup the interface of Userlist module

--- lib/battleships/userlist.ex
+++ lib/battleships/userlist.ex
@@ -14,21 +14,31 @@ defmodule Battleships.Userlist do
   end
 
   @spec get() :: userlist()
-  def get() do
-    GenServer.call(__MODULE__, :get)
-  end
+  def get(), do: GenServer.call(__MODULE__, :get)
 
-  @spec add(String.t()) :: :ok | :in_use
-  def add(new_user), do: GenServer.call(__MODULE__, {:add, new_user})
+  @spec join(String.t()) :: :ok | :in_use
+  def join(new_user), do: GenServer.call(__MODULE__, {:join, new_user})
+
+  @spec leave(String.t()) :: :ok | :unknown_user
+  def leave(user), do: GenServer.call(__MODULE__, {:leave, user})
 
   @spec handle_call(:get, any(), state()) :: {:reply, userlist(), state()}
   def handle_call(:get, _from, state), do: {:reply, state |> MapSet.to_list(), state}
 
-  @spec handle_call({:add, String.t()}, any(), state()) :: {:reply, :ok | :in_use, state()}
-  def handle_call({:add, new_user}, _from, state) do
+  @spec handle_call({:join, String.t()}, any(), state()) :: {:reply, :ok | :in_use, state()}
+  def handle_call({:join, new_user}, _from, state) do
     case state |> MapSet.member?(new_user) do
       true -> {:reply, :in_use, state}
       false -> {:reply, :ok, state |> MapSet.put(new_user)}
+    end
+  end
+
+  @spec handle_call({:leave, String.t()}, any(), state()) ::
+          {:reply, :ok | :unknown_user, state()}
+  def handle_call({:leave, user}, _from, state) do
+    case state |> MapSet.member?(user) do
+      true -> {:reply, :ok, state |> MapSet.delete(user)}
+      false -> {:reply, :unknown_user, state}
     end
   end
 end
--- test/battleship/userlist_test.exs
+++ test/battleship/userlist_test.exs
@@ -15,14 +15,27 @@ defmodule Battleship.UserlistTest do
     assert Battleships.Userlist.get() == []
   end
 
-  test "that you can insert users" do
-    assert :ok == Battleships.Userlist.add("user1")
+  test "that a user can join" do
+    assert :ok == Battleships.Userlist.join("user1")
     assert Battleships.Userlist.get() == ["user1"]
   end
 
-  test "that you cannot insert the same user twice" do
-    assert :ok == Battleships.Userlist.add("user1")
-    assert :in_use == Battleships.Userlist.add("user1")
+  test "that the same user cannot join twice" do
+    assert :ok == Battleships.Userlist.join("user1")
+    assert :in_use == Battleships.Userlist.join("user1")
     assert Battleships.Userlist.get() == ["user1"]
+  end
+
+  test "that users can leave" do
+    Battleships.Userlist.join("user1")
+    assert Battleships.Userlist.leave("user1") == :ok
+    assert Battleships.Userlist.get() == []
+  end
+
+  test "that a user won't be able to leave twice" do
+    Battleships.Userlist.join("user1")
+    assert Battleships.Userlist.leave("user1") == :ok
+    assert Battleships.Userlist.leave("user1") == :unknown_user
+    assert Battleships.Userlist.get() == []
   end
 end
